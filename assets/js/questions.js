857b29fff7809485162b29917d85467d217d4697
rhiannonmcn <rhiannonmcn@gmail.com>
2022-02-26 19:28:27 +0000

    Update questions.js with more questions for a total of 30 in each difficulty

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -91,14 +91,14 @@ const easyQuestions = [{
     correctAnswer: "Mischief Managed"
 },
 {
-    question: "What was Lily Potter's surname before she marries James Potter?",
+    question: "What plant did Harry Potter eat in the second task of the Goblet of Fire?",
     answers: [
-        {text: "Evans"},
-        {text: "Smith"},
-        {text: "Peters"},
-        {text: "Cullen"}
+        {text: "Fluxweed"},
+        {text: "Gillyweed"},
+        {text: "Asphodel"},
+        {text: "Mandrake"}
     ],
-    correctAnswer: "Evans"
+    correctAnswer: "Gillyweed"
 },
 {
     question: "What was 'prongs' real name?",
@@ -149,7 +149,158 @@ const easyQuestions = [{
         {text: "Werewolves"}
     ],
     correctAnswer: "Thestrals"
-}];
+},
+{
+    question: "What charm illuminates the tip of the caster's wand?",
+    answers: [
+        {text: "Lumos"},
+        {text: "Lumen"},
+        {text: "Lumiere"},
+        {text: "Lux"}
+    ],
+    correctAnswer: "Lumos"
+},
+{
+    question: "What is Ron Weasley's pet rat's name?",
+    answers: [
+        {text: "Pig"},
+        {text: "Scabbers"},
+        {text: "Fang"},
+        {text: "Dobby"}
+    ],
+    correctAnswer: "Scabbers"
+},
+{
+    question: "What Quidditch position does Harry Potter play?",
+    answers: [
+        {text: "Keeper"},
+        {text: "Chaser"},
+        {text: "Beater"},
+        {text: "Seeker"}
+    ],
+    correctAnswer: "Seeker"
+},
+{
+    question: "Who is the Bulgarian Quidditch teams seeker?",
+    answers: [
+        {text: "Viktor Krum"},
+        {text: "Aidan Lynch"},
+        {text: "Galvin Gudgeon"},
+        {text: "Gwenog Jones"}
+    ],
+    correctAnswer: "Viktor Krum"
+},
+{
+    question: "Who tells Harry Potter that he's a wizard?",
+    answers: [
+        {text: "Albus Dumbledore"},
+        {text: "Minerva McGonagall"},
+        {text: "Hagrid"},
+        {text: "Severus Snape"}
+    ],
+    correctAnswer: "Hagrid"
+},
+{
+    question: "Who is Hermion Granger's halk kneazle's name?",
+    answers: [
+        {text: "Crookshanks"},
+        {text: "Trevor"},
+        {text: "Pig"},
+        {text: "Scabbers"}
+    ],
+    correctAnswer: "Crookshanks"
+},
+{
+    question: "Who gifts Haryy Potter his Firebolt broom?",
+    answers: [
+        {text: "Albus Dumbledore"},
+        {text: "Minerva McGonagall"},
+        {text: "Oliver Wood"},
+        {text: "Sirius Black"}
+    ],
+    correctAnswer: "Sirius Black"
+},
+{
+    question: "Who was Severus Snape in love with?",
+    answers: [
+        {text: "Lily Potter"},
+        {text: "Petunia Dursley"},
+        {text: "Hestia Jones"},
+        {text: "Amelia Bones"}
+    ],
+    correctAnswer: "Lily Potter"
+},
+{
+    question: "From which platform at King's Cross does the Hogwarts Express depart?",
+    answers: [
+        {text: "9 and 3 quarters"},
+        {text: "10 and a half"},
+        {text: "9"},
+        {text: "10 and 4 thirds"}
+    ],
+    correctAnswer: "9 and 3 quarters"
+},
+{
+    question: "What shape is Harry Potter's scar on his forehead?",
+    answers: [
+        {text: "Star"},
+        {text: "Wand"},
+        {text: "Lightening bolt"},
+        {text: "Cauldron"}
+    ],
+    correctAnswer: "Lightening bolt"
+},
+{
+    question: "On a quidditch pitch, how many goal posts are there in total?",
+    answers: [
+        {text: "3"},
+        {text: "6"},
+        {text: "4"},
+        {text: "8"}
+    ],
+    correctAnswer: "6"
+},
+{
+    question: "What does Hagrid name his baby dragon ?",
+    answers: [
+        {text: "Norbert"},
+        {text: "Fluffy"},
+        {text: "Fang"},
+        {text: "Pig"}
+    ],
+    correctAnswer: "Norbert"
+},
+{
+    question: "What does Hagrid gift Harry Potter in Diagon Alley on their first trip?",
+    answers: [
+        {text: "Toad"},
+        {text: "Eagle"},
+        {text: "Owl"},
+        {text: "Kneazle"}
+    ],
+    correctAnswer: "Owl"
+},
+{
+    question: "Who runs the Gringotts Wizarding bank in Diagon Alley?",
+    answers: [
+        {text: "House Elves"},
+        {text: "Goblins"},
+        {text: "Centaurs"},
+        {text: "Leprachauns"}
+    ],
+    correctAnswer: "Goblins"
+},
+{
+    question: "What is Flich's pet cat's name?",
+    answers: [
+        {text: "Mrs. Norris"},
+        {text: "Mrs. Figg"},
+        {text: "Mrs. Weasley"},
+        {text: "Mrs. Polkiess"}
+    ],
+    correctAnswer: "Mrs. Norris"
+}
+];
 
 const mediumQuestions = [{
     question: "What is Fleur Delacour's sister's name?",
@@ -170,6 +321,298 @@ const mediumQuestions = [{
         {text: "Richard"}
     ],
     correctAnswer: "Arnold"
+}
+,
+{
+    question: "What are Neville Longbottom's parent's names?",
+    answers: [
+        {text: "Frank & Alice"},
+        {text: "Arnold & Louise"},
+        {text: "Mike & Jane"},
+        {text: "James & Dot"}
+    ],
+    correctAnswer: "Frank & Alice"
+},
+{
+    question: "What ahouse was Sirius Black sorted into?",
+    answers: [
+        {text: "Slytherin"},
+        {text: "Ravenclaw"},
+        {text: "Gryffindor"},
+        {text: "Hufflepuff"}
+    ],
+    correctAnswer: "Gryffindor"
+},
+{
+    question: "What kind of dragon did Harry Potter face in the First Task?",
+    answers: [
+        {text: "Hungarian Horntail"},
+        {text: "Chinese Fireball"},
+        {text: "Welsh Green"},
+        {text: "Swedish Short Snout"}
+    ],
+    correctAnswer: "Hungarian Horntail"
+},
+{
+    question: "What does Prof. Lupin offer Harry Potter after the dementor attack on the train?",
+    answers: [
+        {text: "A potion"},
+        {text: "Pumpkin Juice"},
+        {text: "Treacle Tart"},
+        {text: "Chocolate"}
+    ],
+    correctAnswer: "Chocolate"
+},
+{
+    question: "What is the name of Hagrid's giant half brother?",
+    answers: [
+        {text: "Goliath"},
+        {text: "Cyclops"},
+        {text: "Fridwulfa"},
+        {text: "Grawp"}
+    ],
+    correctAnswer: "Grawp"
+},
+{
+    question: "Who tells Harry Potter that he's a horcrux?",
+    answers: [
+        {text: "Severus Snape"},
+        {text: "Albus Dumbledore"},
+        {text: "Minerva McGonagall"},
+        {text: "Sirius Black"}
+    ],
+    correctAnswer: "Albus Dumbledore"
+},
+{
+    question: "What is the name of Hagrid's giant pet spider?",
+    answers: [
+        {text: "Fang"},
+        {text: "Buckbeak"},
+        {text: "Norbert"},
+        {text: "Aragog"}
+    ],
+    correctAnswer: "Aragog"
+}
+,
+{
+    question: "What is Harry Potter's date of birth?",
+    answers: [
+        {text: "31st July 1980"},
+        {text: "30th July 1980"},
+        {text: "31st June 1981"},
+        {text: "30th July 1981"}
+    ],
+    correctAnswer: "31st July 1980"
+},
+{
+    question: "Who hosts the radio show Potterwatch?",
+    answers: [
+        {text: "Fred & George Weasley"},
+        {text: "Dean Thomas"},
+        {text: "Seamus Finnegan"},
+        {text: "Lee Jordan"}
+    ],
+    correctAnswer: "Lee Jordan"
+},
+{
+    question: "Where does Dudley Dursley go to school?",
+    answers: [
+        {text: "Smeltings"},
+        {text: "St. Brutus"},
+        {text: "Hogwarts"},
+        {text: "Wools"}
+    ],
+    correctAnswer: "Smeltings"
+},
+{
+    question: "How fast can a firebolt reach?",
+    answers: [
+        {text: "140mph"},
+        {text: "150mph"},
+        {text: "160mph"},
+        {text: "170mph"}
+    ],
+    correctAnswer: "150mph"
+},
+{
+    question: "What is Ron Weasley's Patronus?",
+    answers: [
+        {text: "Jack Russell Terrier"},
+        {text: "Labrador"},
+        {text: "Collie"},
+        {text: "Greyhound"}
+    ],
+    correctAnswer: "Jack Russell Terrier"
+},
+{
+    question: "What is the name of the centaur who became a divination teacher in Haryy Potter's 6th year",
+    answers: [
+        {text: "Magorian"},
+        {text: "Bane"},
+        {text: "Ronan"},
+        {text: "Firenze"}
+    ],
+    correctAnswer: "Firenze"
+},
+{
+    question: "What portkey took Harry Potter to the Quidditch World Cup?",
+    answers: [
+        {text: "Old rubber tyre"},
+        {text: "Old boot"},
+        {text: "Punctured football"},
+        {text: "Empty drink can"}
+    ],
+    correctAnswer: "Old boot"
+},
+{
+    question: "How did Moaning Myrtle die",
+    answers: [
+        {text: "Mountain troll"},
+        {text: "Killing Curse"},
+        {text: "Basilisk"},
+        {text: "Acromantula"}
+    ],
+    correctAnswer: "Basilisk"
+},
+{
+    question: "What potion does Harry Potter pretend to gove Ron Weasley during a Quidditch match?",
+    answers: [
+        {text: "Draught of Peace"},
+        {text: "Amortentia"},
+        {text: "Pepperup"},
+        {text: "Felix Felicis"}
+    ],
+    correctAnswer: "Felix Felicis"
+},
+{
+    question: "What does Lavender Brown call Ron Weasley when they are dating?",
+    answers: [
+        {text: "Ronnikens"},
+        {text: "Won-Won"},
+        {text: "Ronnie"},
+        {text: "Ron-Won"}
+    ],
+    correctAnswer: "Won-Won"
+},
+{
+    question: "What is Reeter Skeeter's animagus form?",
+    answers: [
+        {text: "Water Beetle"},
+        {text: "Dung Beetle"},
+        {text: "Beetle"},
+        {text: "Stag Beetle"}
+    ],
+    correctAnswer: "Beetle"
+},
+{
+    question: "Where can a bezoar be found?",
+    answers: [
+        {text: "Goat's stomach"},
+        {text: "Cow's stomach"},
+        {text: "Snake's stomach"},
+        {text: "Wolf's stomach"}
+    ],
+    correctAnswer: "Goat's stomach"
+},
+{
+    question: "What other wicth/wizard could the prophecy have been about?",
+    answers: [
+        {text: "Dean Thomas"},
+        {text: "Luna Lovegood"},
+        {text: "Susan Bones"},
+        {text: "Neville Longbottom"}
+    ],
+    correctAnswer: "Neville Longbottom"
+},
+{
+    question: "Who is the Half-Blood Prince?",
+    answers: [
+        {text: "Eileen Prince"},
+        {text: "Severus Snape"},
+        {text: "Lucious Malfoy"},
+        {text: "Tom Riddle"}
+    ],
+    correctAnswer: "Severus Snape"
+},
+{
+    question: "Who became a professional Quidditch player?",
+    answers: [
+        {text: "Harry Potter"},
+        {text: "Ron Weasley"},
+        {text: "Ginny Weasley"},
+        {text: "Charlie Weasley"}
+    ],
+    correctAnswer: "Ginny Weasley"
+},
+{
+    question: "What position did James Potter play for his Quidditch House team?",
+    answers: [
+        {text: "Seeker"},
+        {text: "Chaser"},
+        {text: "Keeper"},
+        {text: "Beater"}
+    ],
+    correctAnswer: "Chaser"
+},
+{
+    question: "At what number house does Vernon and Petunia Dursley live?",
+    answers: [
+        {text: "4"},
+        {text: "5"},
+        {text: "6"},
+        {text: "7"}
+    ],
+    correctAnswer: "4"
+},
+{
+    question: "What is Hermione Granger's parent's profession?",
+    answers: [
+        {text: "Doctors"},
+        {text: "Hygienists"},
+        {text: "Nurses"},
+        {text: "Dentists"}
+    ],
+    correctAnswer: "Dentists"
+},
+{
+    question: "How old was Tom Riddle when he first opened the Chamber of Secrets?",
+    answers: [
+        {text: "15"},
+        {text: "16"},
+        {text: "17"},
+        {text: "18"}
+    ],
+    correctAnswer: "16"
+},
+{
+    question: "In what year did Harry Potter go on  a date with Cho Chang?",
+    answers: [
+        {text: "4th year"},
+        {text: "5th year"},
+        {text: "6th year"},
+        {text: "7th year"}
+    ],
+    correctAnswer: "5th year"
+},
+{
+    question: "What spell is a full body bind?",
+    answers: [
+        {text: "Alohomora"},
+        {text: "Expecto Patronun"},
+        {text: "Lumos"},
+        {text: "Petrifiuc Totalus"}
+    ],
+    correctAnswer: "Petrificus Totalus"
+},
+{
+    question: "Who teaches astronomy at Hogwarts?",
+    answers: [
+        {text: "Charity Burbage"},
+        {text: "Aurora Sinistra"},
+        {text: "Alecto Carrow"},
+        {text: "Sybill Trelawney"}
+    ],
+    correctAnswer: "Aurora Sinistra"
 }
 ];
 
@@ -192,4 +635,287 @@ const hardQuestions = [{
         {text: "Richard"}
     ],
     correctAnswer: "Astoria Greengrass"
-}];
+},
+{
+    question: "What year was Moaning Myrtle killed in?",
+    answers: [
+        {text: "1940"},
+        {text: "1948"},
+        {text: "1943"},
+        {text: "1945"}
+    ],
+    correctAnswer: "1943"
+},
+{
+    question: "What are Severus Snapes' parent's names?",
+    answers: [
+        {text: "Theo & Ethel"},
+        {text: "Toby & Ellen"},
+        {text: "Tom & Elaina"},
+        {text: "Tobias & Eileen"}
+    ],
+    correctAnswer: "Tobias & Eileen"
+},
+{
+    question: "What does Harry Potter smell when he sniffs Amortentia",
+    answers: [
+        {text: "Freshly Mown Grass"},
+        {text: "Treacle Tart"},
+        {text: "New Parchment"},
+        {text: "Fresh Air"}
+    ],
+    correctAnswer: "Treacle Tart"
+},
+{
+    question: "What is Hermione Granger's patronus?",
+    answers: [
+        {text: "Otter"},
+        {text: "Labrador"},
+        {text: "Swan"},
+        {text: "Horse"}
+    ],
+    correctAnswer: "Otter"
+},
+{
+    question: "Who told Harry Potter he had to die to get rid of the horcrux?",
+    answers: [
+        {text: "Albus Dumbledore"},
+        {text: "Sirius Black"},
+        {text: "Minerva McGonagall"},
+        {text: "Severus Snape"}
+    ],
+    correctAnswer: "Severus Snape"
+},
+{
+    question: "On what day are Fred & George Weasley born?",
+    answers: [
+        {text: "April 2nd"},
+        {text: "April 31st"},
+        {text: "April 15th"},
+        {text: "April 1st"}
+    ],
+    correctAnswer: "April 1st"
+},
+{
+    question: "What house was Tonks sortede into?",
+    answers: [
+        {text: "Hufflepuff"},
+        {text: "Gryffindor"},
+        {text: "Slytherin"},
+        {text: "Ravenclaw"}
+    ],
+    correctAnswer: "Hufflepuff"
+},
+{
+    question: "How many presents did Dudley Dursley get on his 11th birthday?",
+    answers: [
+        {text: "35"},
+        {text: "36"},
+        {text: "37"},
+        {text: "38"}
+    ],
+    correctAnswer: "36"
+}
+,
+{
+    question: "How is Bellatrix Lestrange related to Draco Malfoy?",
+    answers: [
+        {text: "Aunt"},
+        {text: "Cousin"},
+        {text: "Aunt in Law"},
+        {text: "2nd Cousin"}
+    ],
+    correctAnswer: "Cousin"
+},
+{
+    question: "How many staircases are their in Hogwarts?",
+    answers: [
+        {text: "140"},
+        {text: "141"},
+        {text: "142"},
+        {text: "143"}
+    ],
+    correctAnswer: "142"
+},
+{
+    question: "What is Albus Dumbledore's scar on his knee a map of?",
+    answers: [
+        {text: "Hogsmeade"},
+        {text: "Diagon Alley"},
+        {text: "Hogwarts"},
+        {text: "London Underground"}
+    ],
+    correctAnswer: "London Underground"
+},
+{
+    question: "What numbers are dialled to activate the entrance to the Ministry of Magic?",
+    answers: [
+        {text: "26442"},
+        {text: "62442"},
+        {text: "24664"},
+        {text: "64226"}
+    ],
+    correctAnswer: "62442"
+},
+{
+    question: "Monkswood and wolfsbane are the same plant, also known as?",
+    answers: [
+        {text: "Aconite"},
+        {text: "Fluxweed"},
+        {text: "Gillyweed"},
+        {text: "knotgrass"}
+    ],
+    correctAnswer: "Aconite"
+},
+{
+    question: "Where does Vernon Dursley work?",
+    answers: [
+        {text: "Surrey Drills"},
+        {text: "Grunnings"},
+        {text: "Smeltings"},
+        {text: "Brutus"}
+    ],
+    correctAnswer: "Grunnings"
+},
+{
+    question: "What colour did Hermione Granger turn the Weasley's crab apple tree for Harry Potter's 17th birthday?",
+    answers: [
+        {text: "red & gold"},
+        {text: "red"},
+        {text: "gold"},
+        {text: "red & yellow"}
+    ],
+    correctAnswer: "gold"
+}
+,
+{
+    question: "What does Albus Dumbledore tell Harry Potter he sees in the Mirror of Erised?",
+    answers: [
+        {text: "His sister"},
+        {text: "His family"},
+        {text: "His brother"},
+        {text: "Socks"}
+    ],
+    correctAnswer: "Socks"
+},
+{
+    question: "What is Voldemort's mother's name?",
+    answers: [
+        {text: "Merope Riddle"},
+        {text: "Millicent Gaunt"},
+        {text: "Merope Gaunt"},
+        {text: "Millicient Merope"}
+    ],
+    correctAnswer: "Merope Riddle"
+},
+{
+    question: "During their first trip to Diagon Alley, what flavour ice cream did Hagrid buy Harry Potter?",
+    answers: [
+        {text: "Chocolate mint"},
+        {text: "Chocolate & Raspberry"},
+        {text: "Raspberry"},
+        {text: "Treacle"}
+    ],
+    correctAnswer: "Chocolate & Raspberry"
+},
+{
+    question: "What is the name of the orphange Tom Riddle was left at?",
+    answers: [
+        {text: "St. Brutus"},
+        {text: "Smeltings"},
+        {text: "Wools"},
+        {text: "Catchpole"}
+    ],
+    correctAnswer: "Wools"
+},
+{
+    question: "What is the name of Aragog's wife?",
+    answers: [
+        {text: "Mosag"},
+        {text: "Grawp"},
+        {text: "Gasog"},
+        {text: "Mogas"}
+    ],
+    correctAnswer: "Mosag"
+},
+{
+    question: "What wood is Lily Potter's wand made of?",
+    answers: [
+        {text: "Birch"},
+        {text: "Ash"},
+        {text: "Cherry"},
+        {text: "Willow"}
+    ],
+    correctAnswer: "Willow"
+},
+{
+    question: "How old was Nicholas Flamel when destroyed the Philosopher's stone?",
+    answers: [
+        {text: "663"},
+        {text: "664"},
+        {text: "665"},
+        {text: "667"}
+    ],
+    correctAnswer: "665"
+},
+{
+    question: "How many possible Quidditch fouls are there?",
+    answers: [
+        {text: "900"},
+        {text: "800"},
+        {text: "700"},
+        {text: "600"}
+    ],
+    correctAnswer: "700"
+},
+{
+    question: "In the Department of Mysteries, what row holds the prophecy about Harry Potter and Voldemort?",
+    answers: [
+        {text: "67"},
+        {text: "77"},
+        {text: "87"},
+        {text: "97"}
+    ],
+    correctAnswer: "97"
+},
+{
+    question: "In the Quidditch world cup, which Irish chaser scored the first goal?",
+    answers: [
+        {text: "Mullet"},
+        {text: "Troy"},
+        {text: "Moran"},
+        {text: "Lynch"}
+    ],
+    correctAnswer: "Troy"
+},
+{
+    question: "What is the name of the Apparition instructor who comes to Hogwarts in Harry Potter's 6th year?",
+    answers: [
+        {text: "Wilkie Twycross"},
+        {text: "Mafalda Hopkirk"},
+        {text: "Reginald Cattermole"},
+        {text: "Bertha Jorkins"}
+    ],
+    correctAnswer: "Wilkie Twycross"
+},
+{
+    question: "What is the name of Hepzibah Smith's house elf?",
+    answers: [
+        {text: "Winky"},
+        {text: "Pokey"},
+        {text: "Hokey"},
+        {text: "Wokey"}
+    ],
+    correctAnswer: "Hokey"
+},
+{
+    question: "What is Fred Weasley's chosen code name on Potterwatch?",
+    answers: [
+        {text: "Rodent"},
+        {text: "Rapier"},
+        {text: "River"},
+        {text: "Royal"}
+    ],
+    correctAnswer: "Rapier"
+}
+];
