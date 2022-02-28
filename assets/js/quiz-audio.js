212cd0e5f978216a0b6c3eaa107918533e6c97be
rhiannonmcn <rhiannonmcn@gmail.com>
2022-02-28 13:43:49 +0000

    Add docstrings to functions in quiz-audio.js

--- assets/js/quiz-audio.js
+++ assets/js/quiz-audio.js
@@ -6,6 +6,11 @@ const audioOnButton = document.getElementsByClassName("audio-on");
 const audioOffButton = document.getElementsByClassName("audio-off");
 let audioPlaying = true;
 
+/**
+ * Let's the player choose to have sounds on or not
+ * Loops through the audio buttons if sound is playing and adds the appropriate class in html
+ * Loops through the audio buttons if soumd is not playing and adds the appropriate class in html
+ */
 function toggleAudio(){
     if(audioPlaying = true){
         for(let i =0; i < audioOnButton.length && audioOffButton.length; i++){
@@ -19,7 +24,9 @@ function toggleAudio(){
         }
     }
 }
-
+/**
+ * Gets the sound for when a player clicks a button if sound is on otherwise it pauses it
+ */
 function buttonSoundEffects(){
     if(audioPlaying){
         buttonClick.play();
@@ -28,6 +35,9 @@ function buttonSoundEffects(){
     }
 }
 
+/**
+ * Gets the sound for when a player gets a right answer if sound is on otherwise it pauses it
+ */
 function rightSoundEffects(){
     if(audioPlaying){
         questionRightSound.play();
@@ -36,6 +46,9 @@ function rightSoundEffects(){
     }
 }
 
+/**
+ * Gets the sound for when a player gets a wrong answer if sound is on otherwise it pauses it
+ */
 function wrongSoundEffects(){
     if(audioPlaying){
         questionWrongSound.play();
