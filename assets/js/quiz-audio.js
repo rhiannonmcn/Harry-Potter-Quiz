dae6aa01f2b36dc31285960450bf6e79ae951cf2
rhiannonmcn <rhiannonmcn@gmail.com>
2022-02-28 12:39:30 +0000

    Add const variables to quiz-audio.js

---
+++ assets/js/quiz-audio.js
@@ -0,0 +1,4 @@
+const questionRightSound = new Audio("assets/sounds/right-sound.mp3");
+const questionWrongSound = new Audio("assets/sounds/wrong-sound.mp3");
+const buttonClick = new Audio("assets/sounds/button-click.mp3");
+const backgroundMusic = new Audio("assets/sounds/background-music");
