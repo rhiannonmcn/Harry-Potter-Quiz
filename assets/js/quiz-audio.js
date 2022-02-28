257f4f8b85062657edbecf55ba3792d1701a7132
rhiannonmcn <rhiannonmcn@gmail.com>
2022-02-28 13:23:07 +0000

    Add toggleAudio function and relevant variables

--- assets/js/quiz-audio.js
+++ assets/js/quiz-audio.js
@@ -2,3 +2,16 @@ const questionRightSound = new Audio("assets/sounds/right-sound.mp3");
 const questionWrongSound = new Audio("assets/sounds/wrong-sound.mp3");
 const buttonClick = new Audio("assets/sounds/button-click.mp3");
 const backgroundMusic = new Audio("assets/sounds/background-music");
+const audioOnButton = document.getElementsByClassName("audio-on");
+const audioOffButton = document.getElementsByClassName("audio-off");
+let audioPlaying = true;
+
+function toggleAudio(){
+    if(audioPlaying = true){
+        for(let i =0; i < audioOnButton.length && audioOffButton.length; i++){
+            audioOnButton[i].classList.remove('hide');
+            audioOffButton[i].classList.add('hide');
+        }
+    }
+}
+
